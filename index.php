e8ad501b26144210efa81dcbf99c633e327da8a6
Frankie Simon <frankie83@gmail.com>
2016-11-30 18:33:41 +0200

    Updated scripts and looks

--- index.php
+++ index.php
@@ -1,26 +1,43 @@
 <html>
 <head>
-    <script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+    <script type="text/javascript" src="lib/jquery-3.1.1.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Crimson+Text" />
+    <style type="text/css" src"css/reset.css"></style>
     <style type="text/css">
         .box {
-            width: 200px;
-            height: 200px;
-            background-color: green;
+            width: 100px;
+            height: 100px;
+            background-color: black;
         }
-        .day {
-            background-color: aqua;
-            text-align: center;                
+        .innerbox {
+            height: 100%;
+            
         }
-        .month {
-            background-color: pink;
+        .day {            
+            padding-top: 4px;       
+            background-color: transparent;
             text-align: center;
+            font-family: 'Crimson Text';
+            font-size: 40pt;
+            color: white;
+        }
+        .month {            
+            margin-top: -5px;
+            background-color: transparent;            
+            font-family: 'Crimson Text';
+            text-align: center;
+            font-size: 14pt;
+            color: white;
+            
         }
     </style>
 </head>
 <body>
     <div class="box">
-        <div class="day" id="dayContainer"></div>    
-        <div class="month" id="monthContainer"></div>
+        <div class="innerbox">
+            <div class="day" id="dayContainer"></div>    
+            <div class="month" id="monthContainer"></div>
+        </div>
     </div>
     
     <script type="text/javascript">
@@ -31,13 +48,13 @@
 
         var date = new Date();
         var day = String(date.getDate());
-        if (day.length == 1) {
-        day = "0" + day;
-        }
+//        if (day.length == 1) {
+//        day = "0" + day;
+//        }
         var monthIndex = date.getMonth();        
 
         $('#dayContainer').html(day);
-        $('#monthContainer').html(monthNames[monthIndex].toUpperCase());
+        $('#monthContainer').html(monthNames[monthIndex].toUpperCase() + '.');
     </script>
 </body>
 </html>
