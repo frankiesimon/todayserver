21a2b2945d13231b3f8f2d6cfb53ab8d22f33d86
Frankie Simon <frankie83@gmail.com>
2017-01-04 11:38:04 +0200

    Font / size / position changes
    Updated font and sizes according to specs, removed unused script part.

--- index.php
+++ index.php
@@ -1,34 +1,36 @@
 <html>
 <head>
     <script type="text/javascript" src="lib/jquery-3.1.1.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Crimson+Text" />
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">    
     <style type="text/css" src"css/reset.css"></style>
     <style type="text/css">
         .box {
-            width: 100px;
-            height: 100px;
+            width: 80px;
+            height: 80px;
             background-color: black;
         }
         .innerbox {
             height: 100%;
-            
+            width: 100%;
         }
         .day {            
-            padding-top: 4px;       
-            background-color: transparent;
+            width: 100%;
+            padding-top: 8px;       
+            background-color: 'red';
             text-align: center;
-            font-family: 'Crimson Text';
-            font-size: 40pt;
-            color: white;
-        }
-        .month {            
-            margin-top: -5px;
-            background-color: transparent;            
-            font-family: 'Crimson Text';
-            text-align: center;
-            font-size: 14pt;
+            font-family: 'Montserrat', sans-serif;
+            font-size: 28pt;
             color: white;
             
+        }
+        .month {            
+            width: 100%;            
+            margin-top: -3px;
+            background-color: transparent;            
+            font-family: 'Montserrat', sans-serif;
+            text-align: center;
+            font-size: 10pt;
+            color: white;
         }
     </style>
 </head>
@@ -41,17 +43,11 @@
     </div>
     
     <script type="text/javascript">
-        var monthNames = ["Jan", "Feb", "Mar",
-        "Apr", "May", "Jun", "Jul",
-        "Aug", "Sep", "Oct",
-        "Nov", "Dec"];
+        var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
         var date = new Date();
         var day = String(date.getDate());
-//        if (day.length == 1) {
-//        day = "0" + day;
-//        }
-        var monthIndex = date.getMonth();        
+        var monthIndex = date.getMonth();
 
         $('#dayContainer').html(day);
         $('#monthContainer').html(monthNames[monthIndex].toUpperCase() + '.');
